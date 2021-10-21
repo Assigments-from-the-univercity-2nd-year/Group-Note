fdd8268308e22b422aeeba54dcf941fb7d334059
Mark Yavorskyi <yavorskiymark@gmail.com>
2021-10-21 08:53:05 +0300

    Add: layout of ListOfNotesFragment.kt

---
+++ app/src/main/java/com/example/groupnote/Fragments/NoteFragment.kt
@@ -0,0 +1,16 @@
+package com.example.groupnote.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [NoteFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class NoteFragment : Fragment() {
+
+}
